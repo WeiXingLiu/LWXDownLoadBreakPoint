e8bf310ee51621312d41b0c07cce9af723082ce2
xingye123 <18503868161@163.com>
2015-08-07 14:19:08 +0800

    Initial Commit

---
+++ MyDownLoadBreakPoint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDownLoadBreakPoint
+//
+//  Created by qianfeng007 on 15/8/7.
+//  Copyright (c) 2015年 刘卫星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
